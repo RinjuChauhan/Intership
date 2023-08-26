408597fbbc77f2450838f270e53670155a53fd65
RinjuChauhan <133479907+RinjuChauhan@users.noreply.github.com>
2023-08-26 11:44:25 +0530

    Add files via upload

--- Pratice4Q.java
+++ Pratice4Q.java
@@ -7,8 +7,8 @@ public class Pratice4Q {
 	public static void main(String[] args) {
 		
 		int m=3, n=3, k=6;
-		int num1[] = {4,6,7,0,0,0};
-		int num2[] = {3,8,9};
+		int num1[] = {1,6,7,0,0,0};
+		int num2[] = {2,3,9};
 		
 		// output = {3,4,6,7,8,9}
 		/* if num1[i] > num2[j] replace + shift + increase j || or as it is			
@@ -17,23 +17,26 @@ public class Pratice4Q {
 		int j =0, temp;
 		for(int i=0; i<num1.length && j<n; i++) {
 			
-			if(num1[i] > num2[j]) {
-				
-				while( k>1) {
-					num1[k-1] = num1[k-2];
-					k--;
-				}
+			if(num1[i] > num2[j]) {          //1,4,6,7,0,0   i=2, 5,4,3,2,
+				int l=5;                              
+				k=6;
+					while( k>1 && i<l) {
+						num1[k-1] = num1[k-2];
+						k--;
+						l--;
+					}
 				
 				temp = num1[i];
 				num1[i] = num2[j];
 				j++;
 			}
+	
 									
 			if(num1[i]==0 && j<n) {	
 					num1[i] = num2[j];
 					j++;				
 				
-					}
+			}
 				
 		}
 		
