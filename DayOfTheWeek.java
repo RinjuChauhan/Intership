a65c30abe6b8716cda2883a504dedfeade1b49d4
RinjuChauhan <133479907+RinjuChauhan@users.noreply.github.com>
2023-10-04 18:54:53 +0530

    Add files via upload

---
+++ DayOfTheWeek.java
@@ -0,0 +1,102 @@
+	/* (Year Code + Month Code + Century Code + Date Number - Leap Year Code) mod 7
+	 */
+
+package com.java.FlipRoboHW1;
+import java.util.*;
+
+public class DayOfTheWeek {
+	
+	public static int fn(int n) {
+		int centuryCode1 = 0; //for 1900
+		int centuryCode2 = 6;  //for 2000
+		int century=0;
+		
+		if(n/100 == 19)
+			{
+			century = 0;
+			}
+		
+		if(n/100 == 20)
+			{
+			century = 6;
+			}
+		return century;
+	}
+
+	public static void main(String[] args) {
+		
+		int year, month, date;
+		
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter year");
+		year = num.nextInt();
+		
+		System.out.println("Enter month");
+		month = num.nextInt();
+		
+		System.out.println("Enter date");
+		date = num.nextInt();
+		
+		int yearCode, monthCode, centuryCode, dateNumber, leapYearCode;
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		map.put(1,0);
+		map.put(2,3);
+		map.put(3,3);
+		map.put(4,6);
+		map.put(5,1);
+		map.put(6,4);
+		map.put(7,6);
+		map.put(8,2);
+		map.put(9,5);
+		map.put(10,0);
+		map.put(11,3);
+		map.put(12,5);
+		
+		Map<Integer, String> map1 = new HashMap<>();
+		
+		map1.put(0, "Sunday");
+		map1.put(1, "Monday");
+		map1.put(2, "Tuesday");
+		map1.put(3, "Wednesday");
+		map1.put(4, "Thursday");
+		map1.put(5, "Friday");
+		map1.put(6, "Saturday");		
+		
+		//YearCode = (YY + (YY div 4)) mod 7   
+			int n = year;			 
+			int r = 0, sum = 0;
+			int i = 1, j=1;
+			while(i<3) {					
+					r = n%10;
+					sum = sum + (r*j); 
+					n = n/10; j = j*10;
+					i++;
+			}
+			yearCode = (sum + (sum/4)) % 7;
+		
+			monthCode = map.get(month);
+			
+			centuryCode = fn(year);
+			System.out.println(centuryCode);
+			
+			dateNumber = date;
+			
+			leapYearCode = 0;
+			
+			if( ((year%4==0) || (year%100==0 && year%400==0)) && 
+					( monthCode == 1 || monthCode == 2) ) {
+				
+					leapYearCode = 1;}
+				
+				
+			
+			String str;
+	//(Year Code + Month Code + Century Code + Date Number - Leap Year Code) mod 7
+			 
+			str = map1.get((yearCode + monthCode + centuryCode + dateNumber - leapYearCode) % 7);
+			
+			System.out.println("Day on " + date + "-"+ month+"-"+year+" is : " +"\""+ str + "\"");
+	}
+}
